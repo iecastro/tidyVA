9822a1db58c9db1cf1de05595632ff5539cd4ee9
Ivan C <ivedcastr@gmail.com>
2018-12-27 11:52:34 -0500

    add states

--- data-raw/states.R
+++ data-raw/states.R
@@ -6,9 +6,4 @@ options(tigris_class = "sf")
 states <- states(cb = TRUE) %>%
   sf::st_transform(4326)
 
-# visn_st <- sf::st_intersection(states, visn)
-
-write_sf(states,"data/states.shp")
-
-states <- read_sf("data/states.shp") %>%
-  use_data()
+use_data(states)
