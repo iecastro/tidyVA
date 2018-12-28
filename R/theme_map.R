817f2b9fb54218066c7e90893ba86b20daca186e
Ivan C <ivedcastr@gmail.com>
2018-12-28 08:46:38 -0500

    add themes

---
+++ R/theme_map.R
@@ -0,0 +1,16 @@
+#' Minimal ggplot theme for plotting sf without axis text
+#'
+#' @param ... defaults are preset
+#'
+#'  @examples
+#'  ggplot() +
+#'  geom_sf(data) +
+#'  theme_map()
+#'
+#' @export
+
+
+theme_map <- function(...){
+  ggplot2::theme_minimal() +
+    ggplot2::theme(axis.text = ggplot2::element_blank())
+}
