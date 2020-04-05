536316a2c1a0a7ccdfd386880891618160e345e0
Ivan C <ivedcastr@gmail.com>
2020-04-04 21:51:52 -0400

    added conus argument

--- R/shift_geo.R
+++ R/shift_geo.R
@@ -3,6 +3,9 @@
 #' @param data The shapefile with the feature geography you want to shift.
 #'              Currently only supports sector and submarket shapefiles.
 #'
+#' @param conus if set to TRUE, will only return the continental US
+#'              Defaults to FALSE.
+#'
 #' @return a [tibble][tibble::tibble-package] of VHA geographies and attributes
 #'
 #' @references \url{ https://github.com/rdinter/rd3albers}
@@ -17,49 +20,58 @@
 #'
 #' @export
 
-shift_geo <- function(data){
+shift_geo <- function(data, conus = FALSE){
 
   # convert to Albers equal area
   proj <-
     "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
 
-  rotate <- function(a){
-    r <- a * pi / 180 #degrees to radians
-    matrix(c(cos(r), sin(r), -sin(r), cos(r)), nrow = 2, ncol = 2)
-  }
-
-  data <- st_transform(data,proj)
+  data <- st_transform(data, proj)
   states <- tidyVA::states %>% st_transform(proj)
   proj_aea <- st_intersection(states, data)
 
-  # extract, then rotate, shrink & move alaska (and reset projection)
-  alaska <- proj_aea %>% filter(.data$STATEFP == "02")
-  alaska_geom <- st_geometry(alaska)
+  if(conus){
 
-  alaska_shift <- alaska_geom + c(-3450000, -2500000)
-  alaska_shift <- (alaska_shift / 2.3) * rotate(-50)
-  alaska_shift <- st_set_crs(alaska_shift,proj)
+    out <- proj_aea %>%
+      filter(!.data$STATEFP %in%
+               c("02","15","72","78","60","66"))
+  } else {
 
-  alaska <- st_set_geometry(alaska,alaska_shift)
+    rotate <- function(a){
+      r <- a * pi / 180 #degrees to radians
+      matrix(c(cos(r), sin(r), -sin(r), cos(r)), nrow = 2, ncol = 2)
+    }
 
-  # extract, then rotate & shift hawaii
-  hawaii <- proj_aea %>% filter(.data$STATEFP == "15")
-  hawaii_geom <- st_geometry(hawaii)
+    # extract, then rotate, shrink & move alaska
+    # (and reset projection)
+    alaska <- proj_aea %>% filter(.data$STATEFP == "02")
+    alaska_geom <- st_geometry(alaska)
 
-  hawaii_shift <- hawaii_geom + c(4250000,-10500)
-  hawaii_shift <- (hawaii_shift * 1.4) *  rotate(-45)
-  hawaii_shift <- st_set_crs(hawaii_shift,proj)
+    alaska_shift <- alaska_geom + c(-3450000, -2500000)
+    alaska_shift <- (alaska_shift / 2.3) * rotate(-50)
+    alaska_shift <- st_set_crs(alaska_shift, proj)
 
-  hawaii <- st_set_geometry(hawaii,hawaii_shift)
+    alaska <- st_set_geometry(alaska, alaska_shift)
 
-  out <- proj_aea %>%
-    filter(!.data$STATEFP %in%
-             c("02","15","72","78","60","66"))
+    # extract, then rotate & shift hawaii
+    hawaii <- proj_aea %>% filter(.data$STATEFP == "15")
+    hawaii_geom <- st_geometry(hawaii)
 
-  out <- rbind(out, alaska, hawaii)
+    hawaii_shift <- hawaii_geom + c(4250000,-10500)
+    hawaii_shift <- (hawaii_shift * 1.4) *  rotate(-45)
+    hawaii_shift <- st_set_crs(hawaii_shift, proj)
 
-  message("Please note: Alaska and Hawaii are being shifted and are not to scale.")
+    hawaii <- st_set_geometry(hawaii, hawaii_shift)
 
-  return(out)
+    out <- proj_aea %>%
+      filter(!.data$STATEFP %in%
+               c("02","15","72","78","60","66"))
+
+    out <- rbind(out, alaska, hawaii)
+
+    message("Please note: Alaska and Hawaii are being shifted and are not to scale.")
+  }
+
+  out
+
 }
-
